55a65c1e74660b7a3e6e270269b2f89857819e53
Yunfei Zhang <yunfeizhang@outlook.com>
2017-05-31 12:06:53 +0800

    Move more funtions to myutils

--- quant/get_big_order.py
+++ quant/get_big_order.py
@@ -8,91 +8,11 @@ import pandas as pd
 import tushare as ts
 import numpy as np
 import matplotlib.pyplot as plt
+from myutils import *
 import sys,getopt,argparse 
 import datetime as dt
 
 
-def analyze_big_order(code_list,date,volume):
-	_code_list=[]
-	_name_list=[]
-	_buy_list=[]
-	_sell_list=[]
-	_abs_list=[]
-	
-	
-	for stock in code_list:
-		#print("="*100)
-		_buy=0;
-		_sell=0;
-		df = ts.get_sina_dd(stock,date=date,vol=volume)  #code,name,time,volume, preprice, type
-		amount_buy=0
-		amount_sell=0
-		amount_neutral=stock
-		if df is None:
-			#print(stock)
-			#print("None")
-			idx=0
-		else:			
-			idx=len(df.name)
-			_code_list.append(stock)
-		while(idx>0):
-			idx -=1
-			if(df.type[idx] == "买盘"):
-				_buy += float(df.volume[idx])*float(df.price[idx])/10000.0
-			if(df.type[idx] == "卖盘"):
-				_sell += float(df.volume[idx])*float(df.price[idx])/10000.0
-
-		if df is not None:
-			_name_list.append(df.name[0])
-			_buy_list.append(_buy)
-			_sell_list.append(_sell)
-			_abs_list.append(float(_buy)-float(_sell))
-	df2=pd.DataFrame({'code':_code_list,'name':_name_list,'buy':_buy_list,'sell':_sell_list,'Abs':_abs_list})
-	df2.sort_values(by='Abs', ascending=False, inplace=True)
-	pd.set_option('display.width', 200)
-	pd.set_option('max_columns',40)
-	if len(df2.Code)==0:
-		print ("There was no big order")
-	else:
-		#print(df2)
-		print('='*100)
-		print ("Sorting by Abs Ascending:")
-		print('='*100)
-		df2.insert(0,'Code',df2.code)
-		df2.insert(1,'Name',df2.name)
-		df2.insert(2,'Buy',df2.buy)
-		df2.insert(3,'Sell',df2.sell)
-		#df2.insert(4,'YYY',df2.abs)
-		print (df2.iloc[:50,:5])
-		print ("Sorting by ABS descending:")
-		print('='*100)
-		df2.sort_values(by='Abs', ascending=True, inplace=True)
-		print (df2.iloc[:50,:5])
-		
-		print ("Sorting by Buy Orders")
-		print('='*100)
-		df2.sort_values(by='Buy', ascending=False, inplace=True)
-		print (df2.iloc[:50,:5])
-		
-		print ("Sorting by Sell Orders\n")
-		print('='*100)
-		df2.sort_values(by='Sell', ascending=False, inplace=True)
-		print (df2.iloc[:50,:5])
-	
-
-	
-
-def getStockList(stock_list):
-#get stock list:
-	STOCK = []
-	#Get list from text files
-	f = open(stock_list)
-	lines=f.readlines()
-	for line in lines:
-		STOCK.append(line[0:6])
-	#print(STOCK)
-	return STOCK
-	
 def usage():
 	print (sys.argv[0] + ' -i stock list file')
 	print (sys.argv[0] + ' -h #get help info')
--- quant/show_big_order.py
+++ quant/show_big_order.py
@@ -10,136 +10,15 @@ import tushare as ts
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mpdates
-import sys,getopt,argparse 
+import sys,getopt,argparse
 import datetime as dt
 import time
-  
-from matplotlib import animation   
+
+from myutils import *
+from matplotlib import animation
 
 
-def analyze_big_order(code_list,date,volume):
-	_code_list=[]
-	_name_list=[]
-	_buy_list=[]
-	_sell_list=[]
-	_abs_list=[]
-	
-	
-	for stock in code_list:
-		#print("="*100)
-		_buy=0;
-		_sell=0;
-		df = ts.get_sina_dd(stock,date=date,vol=volume)  #code,name,time,volume, preprice, type
-		amount_buy=0
-		amount_sell=0
-		amount_neutral=0
-		if df is None:
-			#print(stock)
-			#print("None")
-			idx=0
-		else:			
-			idx=len(df.name)
-			_code_list.append(stock)
-		while(idx>0):
-			idx -=1
-			if(df.type[idx] == "买盘"):
-				_buy += float(df.volume[idx])*float(df.price[idx])/10000.0
-			if(df.type[idx] == "卖盘"):
-				_sell += float(df.volume[idx])*float(df.price[idx])/10000.0
 
-		if df is not None:
-			_name_list.append(df.name[0])
-			_buy_list.append(_buy)
-			_sell_list.append(_sell)
-			_abs_list.append(float(_buy)-float(_sell))
-	df2=pd.DataFrame({'code':_code_list,'name':_name_list,'buy':_buy_list,'sell':_sell_list,'Abs':_abs_list})
-	df2.sort_values(by='Abs', ascending=False, inplace=True)
-	pd.set_option('display.width', 200)
-	pd.set_option('max_columns',40)
-	#print(df2)
-	print('='*100)
-	print ("Sorting by Buy Orders")
-	print('='*100)
-	df2.insert(0,'Code',df2.code)
-	#df2.insert(1,'Name',df2.name)
-	df2.insert(1,'Buy',df2.buy)
-	df2.insert(2,'Sell',df2.sell)
-	#df2.insert(4,'YYY',df2.abs)
-	print (df2.iloc[:20,:3])
-	'''
-	print ("Sorting by Buy Orders")
-	print('='*100)
-	df2.sort_values(by='Buy', ascending=False, inplace=True)
-	print (df2.iloc[:10,:4])
-	
-	print ("Sorting by Sell Orders\n")
-	print('='*100)
-	df2.sort_values(by='Sell', ascending=False, inplace=True)
-	print (df2.iloc[:10,:4])
-	'''
-
-	
-
-def getStockList(stock_list):
-#get stock list:
-	STOCK = [
-	"600522",
-	"002230",
-	"600487"
-	]
-	#Get list from text files
-	f = open(stock_list)
-	lines=f.readlines()
-	for line in lines:
-		if line[0:6] not in STOCK:
-			#print(line)
-			STOCK.append(line[0:6])
-	#print(STOCK)
-	return STOCK
-
-def getDataUpdate (stock_selected,date,volume):
-	df = ts.get_sina_dd(stock_selected,date,vol=volume)
-	_time=['9:30:0']
-	_volume=[0]
-	all_time=[]
-	all_volume=[]
-	buy_time=[]
-	buy_volume=[]
-	sell_time=[]
-	sell_volume=[]
-	if df is None:
-		idx=0
-	else:			
-		idx=len(df.time)
-	i=0
-	while(idx>0):
-		idx -=1
-		
-		if(df.type[idx] == "买盘"):
-			i=i+1
-			buy_time.append(df.time[idx])
-			buy_volume.append(df.volume[idx])
-			_time.append(df.time[idx])
-			_volume.append(df.volume[idx])
-			_volume[i]=float(_volume[i])+float(_volume[i-1])
-			all_time.append(df.time[idx])
-			all_volume.append(df.volume[idx])
-			#_buy += float(df.volume[idx])*float(df.price[idx])/10000.0
-		elif(df.type[idx] == "卖盘"):
-			i=i+1
-			sell_time.append(df.time[idx])
-			sell_volume.append(0-df.volume[idx])
-			_time.append(df.time[idx])
-			_volume.append(0-float(df.volume[idx]))
-			_volume[i]=float(_volume[i])+float(_volume[i-1])
-			all_time.append(df.time[idx])
-			all_volume.append(0-float(df.volume[idx]))
-	df1=pd.DataFrame({'time':_time,'volume':_volume})	
-	df2=pd.DataFrame({'time':all_time,'volume':all_volume})
-	#print(df2)
-	df3=pd.DataFrame({'time':buy_time,'volume':buy_volume})		
-	df4=pd.DataFrame({'time':sell_time,'volume':sell_volume})
-	return df1,df2,df3,df4
 
 def usage():
 	print (sys.argv[0] + ' -i stock list file')
@@ -178,8 +57,6 @@ if __name__ == '__main__':
 			print ("There was no big order")
 			sys.exit()
 		
-		
-	
 
 		fig = plt.figure() 
 		ax1 = fig.add_subplot(2,1,1, ylim=(min(df1.volume), max(df1.volume)))
