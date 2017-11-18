72cbfa3c7c842421e44d21e18ca6ca92166cd7e9
Yunfei Zhang <yunfeizhang@outlook.com>
2017-11-18 18:19:16 +0800

    Update xticks of the plot to date type V0.011

--- quant/ichimoku/ichimoku_test.py
+++ quant/ichimoku/ichimoku_test.py
@@ -7,9 +7,13 @@ from util import get_data, plot_data
 from pandas import DataFrame, Series
 from technical_analysis import ichimoku
 from datetime import datetime, timedelta,date
+import os
+import time
+import sys
+import getopt,argparse
 
 
-def test_run():
+def test_run(stock='000725'):
 
 	duration = 360
 	#now=datetime.now()
@@ -18,11 +22,26 @@ def test_run():
 	#print(str(n)+" days ago:\n"+str(ndays_ago))
 	start_date=str(ndays_ago)
 	end_date =str(today)
-	stock='000725'
+
 
 	df = get_data(stock,start_date, end_date)
-	plot_data(ichimoku(df))
+	plot_data(df,ichimoku(df['close']),title=stock)
 
 
-if __name__ == "__main__":
-	test_run()
+def usage():
+	print (sys.argv[0] + ' -s stock id')
+	
+
+if __name__ == '__main__':
+	opts, args = getopt.getopt(sys.argv[1:], "s:")
+	stock_list=''
+	single_stock=False
+	stock_selected="002281"
+	for op, value in opts:
+		if op == '-s':
+			stock_selected = value
+		elif op == '-h':
+			usage()
+			sys.exit()
+	
+	test_run(stock_selected)
--- quant/ichimoku/util.py
+++ quant/ichimoku/util.py
@@ -11,24 +11,21 @@ import tushare as ts
 
 def get_data(symbols, start,end):
     df=ts.get_k_data(symbols,start,end)
-    #stocks=df.rename(index=str, columns={"date":"Datetime","close":"Close","high":"High","low":"Low","open":"Open","volume":"Volume"}) 
-    #df.index=df['date']
-    #print(df)
-    return df['close']
+    return df
 
-def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
+def plot_data(df1,df, title="Stock prices", xlabel="Date", ylabel="Price"):
     """Plot stock prices with a custom title and meaningful axis labels."""
     ax = df.plot(title=title, fontsize=12)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
+    
     #ichimoku fill between
-    print(df.index)
-
     plt.fill_between(df.index, df['spana'], df['spanb'], where=df['spanb'] >= df['spana'], facecolor='red', interpolate=True)
     plt.fill_between(df.index, df['spana'], df['spanb'], where=df['spanb'] <= df['spana'], facecolor='green', interpolate=True)
+
+   
+    df2=df1[df.index%25==0]
     plt.grid(True)
-    print(df.index)
-    
-    #plt.gca().set_color_cycle(['yellowgreen','cyan','black','magenta','green','red'])
+    plt.xticks(df2.index, df2['date'])
     plt.show()
 
