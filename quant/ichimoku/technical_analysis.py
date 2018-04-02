f525a62ad1f282918fb27997a69ec45854cbd9f1
Yunfei Zhang <yunfeizhang@outlook.com>
2018-04-02 08:54:56 +0800

    Add comments for ichimouku

--- quant/ichimoku/technical_analysis.py
+++ quant/ichimoku/technical_analysis.py
@@ -1,7 +1,6 @@
 import numpy as np
 from pandas import DataFrame, Series
 
-
 def hhv(s, n):
     return Series.rolling(s, n).max()
 
@@ -12,11 +11,20 @@ def llv(s, n):
 #ichimoku
 def ichimoku(s, n1=9, n2=26, n3=52):
 
+    #average of N-day high and N-day low
     conv = (hhv(s, n1) + llv(s, n1)) / 2
-    base = (hhv(s, n2) + llv(s, n2)) / 2
 
+    #mid point of the latest 26 days
+    base = (hhv(s, n2) + llv(s, n2)) / 2
+    
+    #mid-point between the first 2 lines,  and plot 26 periods ahead
     spana = (conv + base) / 2
+    
+    #mid-point between the 52-period low and  52-period high, and plot 26 periods
     spanb = (hhv(s, n3) + llv(s, n3)) / 2
+    
     k = s
+
+    #Lspan is closing price, and plot 26 periods in the past
     return DataFrame(dict(k=k,conv=conv, base=base, spana=spana.shift(n2),
                           spanb=spanb.shift(n2), lspan=s.shift(-n2)))
